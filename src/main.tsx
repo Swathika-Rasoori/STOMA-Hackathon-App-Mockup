ab37553012c8aa070da6ebc47218173656b88978
Swathika-Rasoori <swathikarasoori06@gmail.com>
2026-02-12 12:21:59 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App";
 import "./styles/index.css";
 import { BrowserRouter } from "react-router-dom";
+import "./i18n";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
