cea234507370ab4acaff2584c235f45ff7c59154
Swathika-Rasoori <swathikarasoori06@gmail.com>
2026-02-12 12:20:42 +0530

    Update index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,1 +1,24 @@
 // i18n config will be added
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import en from "./en.json";
+import hi from "./hi.json";
+import te from "./te.json";
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      hi: { translation: hi },
+      te: { translation: te },
+    },
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
